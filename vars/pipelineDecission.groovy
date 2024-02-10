4201b058d92f2de8dd7a1df3540f5bf0d13f6082
gspvsr <153313380+gspvsr@users.noreply.github.com>
2024-02-10 16:25:52 -0600

    roboshp-shared-libraries

--- vars/pipelineDecission.groovy
+++ vars/pipelineDecission.groovy
@@ -1,7 +1,7 @@
 #!groovy
 
-def decidePipline(Map configMap){
-    echo "I need to take decission based on the map you sent"
+def decidePipeline(Map configMap){
+echo "I need to take decission based on the map you sent"
 }
 
-
+pipelineDecision.decidePipeline(configMap)
