4963e130d4905d89fd468440c230376602136073
gspvsr <153313380+gspvsr@users.noreply.github.com>
2024-02-10 18:41:05 -0600

    roboshp-shared-libraries

--- vars/pipelineDecision.groovy
+++ vars/pipelineDecision.groovy
@@ -1,7 +1,18 @@
 #!groovy
 
 def decidePipeline(Map configMap){
-echo "I need to take decission based on the map you sent"
+    application = configMAP.get("application")
+    //herew we are getting nodeJSVM
+    switch (application){
+        case 'nodeJSVM':
+            echo "application is node JS and VM based"
+            // nodeJSVMCI(configMap)
+            break
+        case 'JavaVM':
+            javaVMCI(configMap)
+            break
+        default:
+            error "Un crecognised application"
+    }
 }
 
-pipelineDecision.decidePipeline(configMap)
