d38f3793553fb0c83bf7ffedb37131b91a9a14a5
gspvsr <153313380+gspvsr@users.noreply.github.com>
2024-03-06 09:05:51 -0600

    added libraries

--- vars/pipelineDecision.groovy
+++ vars/pipelineDecision.groovy
@@ -1,23 +1,25 @@
 #!groovy
 
-def decidePipeline(Map configMap){
+def decidePipleine(Map configMap){
     application = configMap.get("application")
-    //here we are getting nodeJSVM
-    switch (application){
+    //# here we are getting nodeJSVM
+    switch(application) {
         case 'nodeJSVM':
-            echo "application is node JS and VM based"
+            echo "application is Node JS and VM based"
             nodeJSVMCI(configMap)
             break
         case 'nodeJSEKS':
-            echo "application is node JS and EKS based"
+            echo "application is Node JS and VM based"
             nodeJSEKS(configMap)
             break
         case 'JavaVM':
             javaVMCI(configMap)
             break
+        case 'javaEKS':
+            javaEKS(configMap)
+            break
         default:
-            error "Un-recognized application"
+            error "Un recognised application"
             break
     }
 }
-
